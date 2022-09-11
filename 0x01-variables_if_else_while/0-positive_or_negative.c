19fac0a204a279cfeb538e3ce27e1fbe2ff167d2
Oluwapelumi Kehinde <pelumiwork@gmail.com>
2022-09-11 08:55:37 -0700

    Correct program for task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,19 +6,19 @@
 
 
 
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
-**/
+*/
 
 int main(void)
 
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	
 	if (n > 0)
 		printf("%d is positive\n", n);
@@ -26,7 +26,6 @@ int main(void)
 		printf("%d is zero\n", n);
 	else 
 		printf("%d is negative\n", n);
-
 
 	return (0);
 
